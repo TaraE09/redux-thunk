9ec8eb22ee27174d0123ca7c899d79c601612fa3
saicharankalakonda <ksaicharan29@gmail.com>
2024-03-10 21:26:09 +0530

    nothing

--- src/redux/reducers/SomeReducer.js
+++ src/redux/reducers/SomeReducer.js
@@ -9,7 +9,7 @@ const initialState = {
       case 'FETCH_DATA_REQUEST':
         return { ...state, isLoading: true, error: null };
       case 'FETCH_DATA_SUCCESS':
-        return { ...state, data: action.payload, isLoading: true };
+        return { ...state, data: action.payload, isLoading: false };
       case 'FETCH_DATA_FAILURE':
         return { ...state, error: action.payload, isLoading: false };
       default:
